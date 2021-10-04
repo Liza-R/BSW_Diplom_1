38718037122150dc0ef7aa50e8ba2b8b5fb99dbb
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-10-04 13:00:37 +0300

    Initial Commit

---
+++ BSW_Diplom_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BSW_Diplom_1
+//
+//  Created by Elizaveta Rogozhina on 04/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
