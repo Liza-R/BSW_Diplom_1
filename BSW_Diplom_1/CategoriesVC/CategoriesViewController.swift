efcb76da12cabc2583bec464dbb05770ff8b3ca5
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-10-05 14:53:03 +0300

    Fix http connection

--- BSW_Diplom_1/CategoriesVC/CategoriesViewController.swift
+++ BSW_Diplom_1/CategoriesVC/CategoriesViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class CategoriesViewController: UIViewController {
-
+    var all_categories: [AllCategoriesInfo.Info_Mass] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        CategoriesLoader().loadCategoriesInfo{ all_categories in
+            self.all_categories = all_categories
+            DispatchQueue.main.async {
+                for i in all_categories{
+                    print(i)
+                }
+            }
+        }
     }
-    
 }
