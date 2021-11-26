934bae9ae7d4b1751aee1d8a78b5c8b5c06ec657
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-11-26 15:32:07 +0300

    Parsing goods

--- BSW_Diplom_1/ToLoad_Alamofire/Structs/GoodsStruct.swift
+++ BSW_Diplom_1/ToLoad_Alamofire/Structs/GoodsStruct.swift
@@ -8,17 +8,33 @@
 import Foundation
 
 class AllGoodsInfo{
-    struct Info_Mass: Decodable{
-        var _7156_219_, _7157_219_, _8003_219_, _8112_219_: Info_Mass_?
+    struct DecodedArray: Decodable {
+        var array: [Info_Mass_]
         
-        private enum CodingKeys: String, CodingKey{
-            case _7156_219_ = "7156_219", _7157_219_ = "7157_219", _8003_219_ = "8003_219", _8112_219_ = "8112_219"
+        private struct DynamicCodingKeys: CodingKey{
+            var stringValue: String
+            init?(stringValue: String) {
+                self.stringValue = stringValue
+            }
+            var intValue: Int?
+            init?(intValue: Int){
+                 return nil
+            }
+        }
+     
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: DynamicCodingKeys.self)
+            var tempArray = [Info_Mass_]()
+            for key in container.allKeys {
+                let decodedObject = try container.decode(Info_Mass_.self, forKey: DynamicCodingKeys(stringValue: key.stringValue)!)
+                tempArray.append(decodedObject)
+            }
+            array = tempArray
         }
     }
     
     struct Info_Mass_: Decodable{
         var name: String,
-            sortOrder: String,
             englishName: String,
             article: String,
             collection: String?,
@@ -31,8 +47,7 @@ class AllGoodsInfo{
             price: String
     }
     struct ProductImages: Decodable{
-        var imageURL: String,
-            sortOrder: String
+        var imageURL: String
     }
     struct Offers: Decodable{
         var size: String,
