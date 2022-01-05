133353903db3ad41336d5a6117e1a8d7ac177b2b
Liza-R <83958561+Liza-R@users.noreply.github.com>
2022-01-05 12:03:37 +0300

    Adding extension and footerView for category table

--- BSW_Diplom_1/CategoriesVC/CategoriesViewController.swift
+++ BSW_Diplom_1/CategoriesVC/CategoriesViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 var id = "292"
 
-class CategoriesViewController: UIViewController {
+class CategoriesViewController: UIViewController{
     
     @IBOutlet weak var categoriesTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        categoriesTable.delegate = self
+        categoriesTable.rowHeight = 100
+        categoriesTable.dataSource = self
+        categoriesTable.tableFooterView = UIView()
         CategoriesViewModel().uploadCategories()
+    }
+}
+
+extension CategoriesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView_Alam: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView_Alam: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView_Alam.dequeueReusableCell(withIdentifier: "categories_Cell", for: indexPath) as! CategoriesTableViewCell
+        cell.categoryImage.image = .actions
+        cell.categoryNameLabel.text = "Text Category"
+        return cell
     }
 }
